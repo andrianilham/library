15dc163aed7f4cdc922a4505b2aec047b870b980
andrianilham <andrianilhampurnomo@gmail.com>
2020-08-05 17:15:14 +0700

    Inisialisasi Repo

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from'react';
+import { Route, Switch, Redirect } from'react-router-dom';
+import AddBook from './components/AddBook';
+import ListBooks from './components/ListBooks';
+
+const Routes = () => (
+<Switch>
+    {/* Route ini menggunakan path /home untuk menampilkan component ListBooks*/}
+    <Route path="/home" component={ListBooks}/>
+    <Route path="/tambah" component={AddBook}/>
+    <Redirect from='/' to='/home' />    
+</Switch>
+);
+ 
+export default Routes;
+
